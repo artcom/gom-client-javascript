862039d96d823a85beb23b8d35540ad932601a06
Guido Lorenz <guido.lorenz@artcom.de>
2014-02-24 11:28:45 +0100

    Reformat code according to airbnb style guide

--- tests/all.js
+++ tests/all.js
@@ -1,58 +1,60 @@
 /* globals define */
 
 define([
-    "gom-client",
-    "intern!bdd",
-    "intern/chai!expect"
+  'gom-client',
+  'intern!bdd',
+  'intern/chai!expect'
 ], function (Gom, bdd, expect) {
-    var afterEach = bdd.afterEach,
-        before = bdd.before,
-        beforeEach = bdd.beforeEach,
-        describe = bdd.describe,
-        it = bdd.it;
+  'use strict';
 
-    describe("GOM Client", function () {
-        var gom,
-            testNode = "/test/gom-client-javascript-" + Date.now(),
-            testAttribute = testNode + ":attribute-" + Date.now();
+  var afterEach = bdd.afterEach,
+      before = bdd.before,
+      beforeEach = bdd.beforeEach,
+      describe = bdd.describe,
+      it = bdd.it;
 
-        before(function () {
-            gom = new Gom("http://gom.staging.t-gallery");
-        });
+  describe('GOM Client', function () {
+    var gom,
+        testNode = '/test/gom-client-javascript-' + Date.now(),
+        testAttribute = testNode + ':attribute-' + Date.now();
 
-        beforeEach(function () {
-            return gom.update(testAttribute, "initial");
-        });
-
-        afterEach(function () {
-            return gom.destroy(testNode);
-        });
-
-        it("can retrieve attributes", function () {
-            return gom.retrieve(testAttribute).then(function (result) {
-                expect(result.attribute.value).to.equal("initial");
-            });
-        });
-
-        it("can update attributes", function () {
-            return gom.update(testAttribute, "updated").then(function () {
-                return gom.retrieve(testAttribute);
-            }).then(function (result) {
-                expect(result.attribute.value).to.equal("updated");
-            });
-        });
-
-        it("can destroy attributes", function () {
-            var deferred = this.async();
-
-            gom.destroy(testAttribute).then(function () {
-                return gom.retrieve(testAttribute);
-            }).then(function () {
-                // Retrieve is expected to fail, see below.
-                deferred.reject();
-            }).catch(deferred.callback(function (error) {
-                expect(error.xhr.status).to.equal(404);
-            }));
-        });
+    before(function () {
+      gom = new Gom('http://gom.staging.t-gallery');
     });
+
+    beforeEach(function () {
+      return gom.update(testAttribute, 'initial');
+    });
+
+    afterEach(function () {
+      return gom.destroy(testNode);
+    });
+
+    it('can retrieve attributes', function () {
+      return gom.retrieve(testAttribute).then(function (result) {
+        expect(result.attribute.value).to.equal('initial');
+      });
+    });
+
+    it('can update attributes', function () {
+      return gom.update(testAttribute, 'updated').then(function () {
+        return gom.retrieve(testAttribute);
+      }).then(function (result) {
+        expect(result.attribute.value).to.equal('updated');
+      });
+    });
+
+    it('can destroy attributes', function () {
+      var deferred = this.async();
+
+      gom.destroy(testAttribute).then(function () {
+        return gom.retrieve(testAttribute);
+      }).then(function () {
+        // Retrieve is expected to fail, see below.
+        deferred.reject();
+      }).catch(deferred.callback(function (error) {
+        expect(error.xhr.status).to.equal(404);
+      }));
+    });
+  });
 });
